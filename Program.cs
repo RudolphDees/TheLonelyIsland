44aa793f237c1898da41b6d3a8c9e5e472f955cf
sirrudolph <rudy_dees@yahoo.com>
2020-06-03 00:13:54 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.ComponentModel;
 using System.ComponentModel.Design;
 using System.Data.Common;
 using System.Security.Cryptography.X509Certificates;
+using System.Reflection;
 
 namespace ConsoleApp1
 {
@@ -11,46 +13,78 @@ namespace ConsoleApp1
 	{
 		static void Main(string[] args)
 		{
-			Random rand = new Random();
-			int environment = 0, specialEvent = 0, shoreline = 0, i = 0, j = 0;
-			int boardSize = 25;
-			bool GG = false;
-			string[] inventory = { "Compass" };
-			List<string> backpack = new List<string>(inventory);
-			Square[,] board = new Square[boardSize, boardSize];
-			Player player = new Player(backpack, 0, 0, GG);
-			for (j = 0; j < boardSize; j++)
-			{
-				for (i = 0; i < boardSize; i++)
-				{
-					int odds = rand.Next(0, 20);
-					if (j == 0)
-						environment = shoreline;
-					else if (odds < 5 && board[i, j - 1].environment < 4 || board[i, j - 1].environment == 0)
-						environment = board[i, j - 1].environment + 1;
-					else environment = board[i, j - 1].environment;
-					if (odds > 16)
-						specialEvent = 1;
-					else specialEvent = 0;
-					board[i, j] = new Square(environment, specialEvent);
-				}
-			}
-			Console.WriteLine("You wake up with a throbbing in your head. You open you eyes to a painfully bright light. The sun is already starting to scorch your skin." + Environment.NewLine + 
-			"You stand up and attempt to get your bearings. Luckily you find a backpack nearby. You decide to look around for away off of the abandoned island. Good Luck.");
-			board[player.locationX, player.locationY].Description(player, board[player.locationX, player.locationY]);
-			for (i = 0; i < 100; i++)
-			{
-				if (player.GG == false)
-				{
-					player.doSomething(board[player.locationX, player.locationY], player);
-					if (player.GG == false)
-						board[player.locationX, player.locationY].Description(player, board[player.locationX, player.locationY]);
-				}
-			}
+			int boardSize = 25, startingY = 0, startingX = 0;
+			string[] startingInventory = { "Compass" };
+			//Create the player object
+			Player player = new Player(startingInventory, startingX, startingY);
+			//Create the game board
+			Square[,] board = Functions.CreateBoard(boardSize);
+			//Begin the game
+			//Functions.Intro();
+			//Loop through the turns 
+			Functions.GameCycle(player, board);
+			
 		}
 	}
 }
-
+public class Functions
+{
+	//Just a silly intro
+	static public void Intro()
+	{
+		Console.WriteLine("You wake up with a throbbing in your head.");
+		Thread.Sleep(2000);
+		Console.WriteLine("You open you eyes to a painfully bright light.");
+		Thread.Sleep(2000);
+		Console.WriteLine("The sun is already starting to scorch your skin.");
+		Thread.Sleep(2000);
+		Console.WriteLine("You stand up and attempt to get your bearings and find a backpack nearby.");
+		Thread.Sleep(2000);
+		Console.WriteLine("You decide to look around for away off of the abandoned island. Good Luck.");
+		Thread.Sleep(2000);
+	}
+	//This is where all the actions are inputted and cause different events to occur
+	static public void GameCycle(Player player, Square[,] board)
+	{
+		board[player.locationX, player.locationY].Description(player, board[player.locationX, player.locationY]);
+		for (int i = 0; i < 100; i++)
+		{
+			if (player.GG == false)
+			{
+				Thread.Sleep(1000);
+				player.doSomething(board[player.locationX, player.locationY], player);
+				Thread.Sleep(1000);
+				if (player.GG == false && player.tryAgain == false)
+					board[player.locationX, player.locationY].Description(player, board[player.locationX, player.locationY]);
+			}
+		}
+	}
+	//This is the random creation of the board
+	static public Square[,] CreateBoard(int boardSize)
+	{
+		Random rand = new Random();
+		int environment = 0, specialEvent = 0, shoreline = 0;
+		Square[,] board = new Square[boardSize, boardSize];
+		for (int j = 0; j < boardSize; j++)
+		{
+			for (int i = 0; i < boardSize; i++)
+			{
+				int odds = rand.Next(0, 20);
+				if (j == 0)
+					environment = shoreline;
+				else if (odds < 5 && board[i, j - 1].environment < 4 || board[i, j - 1].environment == 0)
+					environment = board[i, j - 1].environment + 1;
+				else environment = board[i, j - 1].environment;
+				if (odds > 16)
+					specialEvent = 1;
+				else specialEvent = 0;
+				board[i, j] = new Square(environment, specialEvent);
+			}
+		}
+		return board;
+	}
+}
+//this is the object that makes up each square of the board
 public class Square
 {
 	public int environment = 0;
@@ -60,6 +94,7 @@ public class Square
 		this.environment = environment;
 		this.specialEvent = specialEvent;
 	}
+	//This describes a specific square
 	public void Description(Player player, Square square)
 	{
 		if (this.environment == 0)
@@ -90,26 +125,29 @@ public class Square
 			}
 		}
 	}
+	//This is what happens when the square causes a random event. Eventually there will be more to this
 	public void RandomEvent(Player player, Square square)
 	{
 		Console.WriteLine("You pick up what appears to be a loaded flare gun with a single round. You put it in your backpack.");
 		player.backpack.Insert(player.backpack.Count, "Flare Gun");
 	}
 }
-
+//This is the player object that hold the players inventory and location and potentially more in the future
 public class Player
 {
 	public List<string> backpack = new List<string>();
 	public int locationX = 0;
 	public int locationY = 0;
 	public bool GG = false;
-	public Player(List<string> backpack, int locationX, int locationY, bool GG)
+	public bool tryAgain = false;
+	public Player(string[] inventory, int locationX, int locationY)
 	{
-		this.backpack = backpack;
+		this.backpack = new List<string>(inventory);
 		this.locationX = locationX;
 		this.locationY = locationY;
-		this.GG = GG;
+		this.GG = false;
 	}
+	//Pretty self expanitory
 	public void Help()
 	{
 		Console.WriteLine(Environment.NewLine + "Here are you're following commands:" + Environment.NewLine +
@@ -122,6 +160,7 @@ public class Player
 			"7) Type the name of anything in your backpack to inspect/use it" + Environment.NewLine +
 			"7) Quit to Quit" + Environment.NewLine);
 	}
+	//This displays the players inventory
 	public void Inventory()
 	{
 		Console.WriteLine(Environment.NewLine + "You open your back pack and find the following...");
@@ -131,9 +170,12 @@ public class Player
 		}
 		Console.WriteLine();
 	}
+	//This is the funtion that actually reads in the users input and could probably use some cleaning up and optimizing
 	public void doSomething(Square square, Player player)
 	{
-		Console.WriteLine("What would you like to do?");
+		if (tryAgain == false)
+			Console.WriteLine("What would you like to do?");
+		tryAgain = false;
 		string move = Console.ReadLine().ToUpper();
 		if (move == "EAST" && this.locationX < 25)
 			this.locationX++;
@@ -170,6 +212,10 @@ public class Player
 			else if (reply == "NO")
 				Console.WriteLine("You put the flare back into your backpack.");
 		}
-		else Console.WriteLine("Try again");
+		else
+		{
+			Console.WriteLine("Try again");
+			tryAgain = true;
+		}
 	}
 }
